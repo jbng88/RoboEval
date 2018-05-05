048ccaed84bb2edc32657dfb2bcac4e78a8ecf5d
Jay Ng <jbng88@OE1010301M.rsccd.org>
2018-05-05 01:15:53 -0700

    jay's changes

--- RoboEval/Controllers/HomeController.cs
+++ RoboEval/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace RoboEval.Controllers
 
             foreach (var course in majorRequirements)
             {
-                if (!coursesTaken.Any(x => x.CourseId == course.CourseId))
+                if (!coursesTaken.Any(x => x.CourseId == course.CourseId)) 
                 {
                     coursesNeeded.Add(course);              
                 }
@@ -43,5 +43,41 @@ namespace RoboEval.Controllers
             //majorCourses.FirstOrDefault(x => x.MajorId == 5);
             //majorCourses.(x => x.MajorId == 5);
         }
+
+        
+        //FUNCTION TO DISPLAY STUDENT'S TRANSCRIPT (LIST COURSES TAKEN)
+        public List<Course> DisplayStudentsTranscript( int studentId, int transcriptId)
+        {
+            var dbContext = new DbContext("RoboEvalEntities");
+
+            var students = dbContext.Database.SqlQuery<Student>("SELECT * FROM Student").ToList();
+            var transcripts = dbContext.Database.SqlQuery<Transcript>("SELECT * FROM Transcript").ToList();
+            var transcriptId = students.FirstOrDefault(student => student.StudentId == studentId).TranscriptId;
+            var coursesTaken = transcripts.FirstOrDefault(x => x.TranscriptId == transcriptId).Courses;
+
+            var studentTranscript = new List<Course>();
+            /*
+            foreach (var  course in transcriptId)       //NEED TO DELCARE THIS ENUMERATOR?
+            {
+                if (coursesTaken.Any(x => x.CourseId == course.CourseId))
+                {
+                    studentTranscript.Add(course);
+                }
+            }
+
+            return studentTranscript;
+          */
+            foreach (var courseId in coursesTaken)
+            {
+                studentTranscript.Add(course);
+            }
+            
+        //    foreach (var courseId in Course)
+        //    {
+          //      studentTranscript.Add(Course);
+         //   }
+          //  return studentTranscript;
+        }
+        
     }
 }

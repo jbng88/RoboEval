bfe3dcded5c1354585f566a6cde3588a5d3b0939
Jay Ng <jbng88@OE1010301M.rsccd.org>
2018-05-05 01:33:09 -0700

    jays's changes

--- RoboEval/Controllers/HomeController.cs
+++ RoboEval/Controllers/HomeController.cs
@@ -19,65 +19,70 @@ namespace RoboEval.Controllers
         //Courses Comparison Logic
         public List<Course> GetAllCoursesStillNeededForMajor(int majorId, int studentId)
         {
-            var dbContext = new DbContext("RoboEvalEntities");
-            //FirstOrDefault to return first row, ToList is the whole list
-            var students = dbContext.Database.SqlQuery<Student>("SELECT * FROM Student").ToList();
-            var major = dbContext.Database.SqlQuery<Major>("SELECT * FROM Major").ToList();
-            var transcripts = dbContext.Database.SqlQuery<Transcript>("SELECT * FROM Transcipt").ToList();
-
-            var majorRequirements = major.FirstOrDefault(x => x.MajorId == majorId).Courses;
-            var transcriptId = students.FirstOrDefault(student => student.StudentId == studentId).TranscriptId;
-            var coursesTaken = transcripts.FirstOrDefault(x => x.TranscriptId == transcriptId).Courses;
-            var coursesNeeded = new List<Course>();
-
-            foreach (var course in majorRequirements)
+            try
             {
-                if (!coursesTaken.Any(x => x.CourseId == course.CourseId)) 
-                {
-                    coursesNeeded.Add(course);              
-                }
-            }
+                var dbContext = new DbContext("RoboEvalEntities");
+                //FirstOrDefault to return first row, ToList is the whole list
+                var students = dbContext.Database.SqlQuery<Student>("SELECT * FROM Student").ToList();
+                var major = dbContext.Database.SqlQuery<Major>("SELECT * FROM Major").ToList();
+                var transcripts = dbContext.Database.SqlQuery<Transcript>("SELECT * FROM Transcipt").ToList();
 
-            return coursesNeeded;
+                var majorRequirements = major.FirstOrDefault(x => x.MajorId == majorId).Courses;
+                var transcriptId = students.FirstOrDefault(student => student.StudentId == studentId).TranscriptId;
+                var coursesTaken = transcripts.FirstOrDefault(x => x.TranscriptId == transcriptId).Courses;
+                var coursesNeeded = new List<Course>();
+
+                foreach (var course in majorRequirements)
+                {
+                    if (!coursesTaken.Any(x => x.CourseId == course.CourseId))
+                    {
+                        coursesNeeded.Add(course);
+                    }
+                }
+
+                return coursesNeeded;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
             //majorCourses.Any(x => x.MajorId == 5);
             //majorCourses.FirstOrDefault(x => x.MajorId == 5);
             //majorCourses.(x => x.MajorId == 5);
         }
 
-        
+
         //FUNCTION TO DISPLAY STUDENT'S TRANSCRIPT (LIST COURSES TAKEN)
-        public List<Course> DisplayStudentsTranscript( int studentId, int transcriptId)
+        public IEnumerable<Course> DisplayStudentsTranscript(int studentId, int transcriptId)
         {
-            var dbContext = new DbContext("RoboEvalEntities");
-
-            var students = dbContext.Database.SqlQuery<Student>("SELECT * FROM Student").ToList();
-            var transcripts = dbContext.Database.SqlQuery<Transcript>("SELECT * FROM Transcript").ToList();
-            var transcriptId = students.FirstOrDefault(student => student.StudentId == studentId).TranscriptId;
-            var coursesTaken = transcripts.FirstOrDefault(x => x.TranscriptId == transcriptId).Courses;
-
-            var studentTranscript = new List<Course>();
-            /*
-            foreach (var  course in transcriptId)       //NEED TO DELCARE THIS ENUMERATOR?
+            try
             {
-                if (coursesTaken.Any(x => x.CourseId == course.CourseId))
-                {
-                    studentTranscript.Add(course);
-                }
-            }
+                var dbContext = new DbContext("RoboEvalEntities");
 
-            return studentTranscript;
-          */
-            foreach (var courseId in coursesTaken)
-            {
-                studentTranscript.Add(course);
+                var students = dbContext.Database.SqlQuery<Student>("SELECT * FROM Student").ToList();
+                var transcripts = dbContext.Database.SqlQuery<Transcript>("SELECT * FROM Transcript").ToList();
+                return transcripts.FirstOrDefault(x => x.TranscriptId == transcriptId)?.Courses;
             }
-            
-        //    foreach (var courseId in Course)
-        //    {
-          //      studentTranscript.Add(Course);
-         //   }
-          //  return studentTranscript;
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
-        
+
+        public void AddClass(Course course)
+        {
+            try
+            {
+                var dbContext = new DbContext("RoboEvalEntities");
+
+                dbContext.Set<Course>().Add(course);
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
